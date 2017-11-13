152de2eb3f324b2678e80f9a0eb8c0bcbae77c2d
Adonis <adonis.lau.work@gmail.com>
2017-11-13 14:12:32 +0800

    reduce

--- bid/adonis/lau/one/reduce.py
+++ bid/adonis/lau/one/reduce.py
@@ -25,3 +25,25 @@ def char2num(s):
 
 
 print(reduce(fn, map(char2num, '123456789')))
+
+
+def str2int(s):
+    def fn(x, y):
+        return x * 10 + y
+
+    def char2num(s):
+        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
+
+    return reduce(fn, map(char2num, s))
+
+
+print(str2int('123456789987654321'))
+
+
+def str2int(s):
+    return reduce(lambda x, y: x * 10 + y, map(char2num, s))
+
+
+print(str2int('852741963'))
+
+
