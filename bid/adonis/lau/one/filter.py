762015a260dda7c11e065767d6063cfd05e243aa
Adonis <adonis.lau.work@gmail.com>
2017-11-27 10:47:27 +0800

    filter

--- bid/adonis/lau/one/filter.py
+++ bid/adonis/lau/one/filter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+
 def is_odd(s):
     return s % 2 == 1
 
@@ -15,6 +16,7 @@ def not_empty(s):
 print(list(filter(not_empty, ['A', '', 'B', None, 'C', '  '])))
 
 
+# 构建一个从3开始的奇数序列
 def _odd_iter():
     n = 1
     while True:
@@ -22,3 +24,41 @@ def _odd_iter():
         yield n
 
 
+# 筛选函数
+def _not_divisible(n):
+    return lambda x: x % n > 0
+
+
+# 素数生成器
+def primes():
+    yield 2
+    it = _odd_iter()
+    while True:
+        n = next(it)
+        yield n
+        it = filter(_not_divisible(n), it)
+
+
+# 打印1000以内的素数
+# 因为 primes() 是一个无线序列，所以调用时需要设置一个退出循环的条件
+for n in primes():
+    if n < 1000:
+        print(n)
+    else:
+        break
+
+
+# 输出回数
+def is_palindrome(n):
+    s = str(n)
+    return s == s[::-1]
+
+
+# 测试:
+output = filter(is_palindrome, range(1, 1000))
+print('1~1000:', list(output))
+if list(filter(is_palindrome, range(1, 200))) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99, 101,
+                                                  111, 121, 131, 141, 151, 161, 171, 181, 191]:
+    print('测试成功!')
+else:
+    print('测试失败!')
